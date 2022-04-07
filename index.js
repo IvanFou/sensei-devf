cdaa09ee4c9163e5732476221ee31d3500db2de7
Ivan Fougerat <57118026+IvanFou@users.noreply.github.com>
2022-04-06 23:23:49 -0500

    Creando y subiendo el repositorio a github

---
+++ index.js
@@ -0,0 +1,40 @@
+const request = new XMLHttpRequest();
+request.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=hrXxLeX5eM53FDGegBcrSeULMQMfPGAyjsq9rYr6', true)
+
+request.onload = function () {
+if (request.status >= 200 && request.status < 400) {
+    const data = JSON.parse(this.response);
+    drawPhotos(data)
+  } else {
+    console.log('Ha ocurrido un error con código ' + request.status);
+  }
+}
+
+request.send()
+
+function drawPhotos(data) {
+    const infoSection = document.querySelector(".info-section")
+
+    const photoContainer = document.createElement("article")    
+    const photoImage = document.createElement("section")
+
+    const imgNasa = document.createElement("img")
+    imgNasa.src = data.hdurl
+
+    const text = document.createElement('p')
+    const imgDescription = document.createTextNode(data.explanation)
+
+    const titulo = document.createElement("h2")
+    const textTittle = document.createTextNode(data.title)
+
+    titulo.appendChild(textTittle)
+    photoContainer.appendChild(titulo)
+
+    text.appendChild(imgDescription)
+    photoContainer.appendChild(text)
+
+    photoImage.appendChild(imgNasa)
+
+    infoSection.appendChild(photoImage)
+    infoSection.appendChild(photoContainer)
+}
